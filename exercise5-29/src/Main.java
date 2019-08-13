830818b947cce857f593db46f7ceb59e11427e82
vasilileliuh <vasili.leliuh@gmail.com>
2019-08-13 10:44:23 +0300

    A better variant of using switch to reduce redundant.

--- exercise5-29/src/Main.java
+++ exercise5-29/src/Main.java
@@ -4,169 +4,87 @@ public class Main {
 
         int day = 1;
         for (int i=1; i<=12; i++){
-//            day *= i;
-//            System.out.println("The product of all integers 1-15 is: " + day);
+
             System.out.println();
+            String dayNumber = new String();
+
         switch (i) {
+
             case 1:
-                System.out.println("On the first day of Christmas");
+                dayNumber = "first";
                 break;
             case 2:
-                System.out.println("On the second day of Christmas");
+                dayNumber = "second";
                 break;
             case 3:
-                System.out.println("On the third day of Christmas");
+                dayNumber = "third";
                 break;
             case 4:
-                System.out.println("On the fourth day of Christmas");
+                dayNumber = "fourth";
                 break;
             case 5:
-                System.out.println("On the fifth day of Christmas");
+                dayNumber = "fifth";
                 break;
             case 6:
-                System.out.println("On the sixth day of Christmas");
+                dayNumber = "sixth";
                 break;
             case 7:
-                System.out.println("On the seventh day of Christmas");
+                dayNumber = "seventh";
                 break;
             case 8:
-                System.out.println("On the eight day of Christmas");
+                dayNumber = "eight";
                 break;
             case 9:
-                System.out.println("On the ninth day of Christmas");
+                dayNumber = "ninth";
                 break;
             case 10:
-                System.out.println("On the tenth day of Christmas");
+                dayNumber = "tenth";
                 break;
             case 11:
-                System.out.println("On the eleven day of Christmas");
+                dayNumber = "eleven";
                 break;
             case 12:
-                System.out.println("On the twelve day of Christmas");
+                dayNumber = "twelve";
                 break;
-
-
-
-
         }
+            System.out.println("On the " + dayNumber + " day of Christmas");
+            System.out.println("My true love gave to me");
+
+//            char firstLetter;
+
         switch (i){
-            case 1:
-                System.out.println("My true love gave to me");
-                System.out.println("A partridge in a pear tree");
-                break;
-            case 2:
-                System.out.println("My true love gave to me");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 3:
-                System.out.println("My true love gave to me");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 4:
-                System.out.println("My true love gave to me");
-                System.out.println("Four calling birds");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 5:
-                System.out.println("My true love gave to me");
-                System.out.println("Five gold rings");
-                System.out.println("Four calling birds");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 6:
-                System.out.println("My true love gave to me");
-                System.out.println("Six geese a laying");
-                System.out.println("Five gold rings");
-                System.out.println("Four calling birds");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 7:
-                System.out.println("My true love gave to me");
-                System.out.println("Seven swans a swimming");
-                System.out.println("Six geese a laying");
-                System.out.println("Five gold rings");
-                System.out.println("Four calling birds");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 8:
-                System.out.println("My true love gave to me");
-                System.out.println("Eight maids a milking");
-                System.out.println("Seven swans a swimming");
-                System.out.println("Six geese a laying");
-                System.out.println("Five gold rings");
-                System.out.println("Four calling birds");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 9:
-                System.out.println("My true love gave to me");
-                System.out.println("Nine ladies dancing");
-                System.out.println("Eight maids a milking");
-                System.out.println("Seven swans a swimming");
-                System.out.println("Six geese a laying");
-                System.out.println("Five gold rings");
-                System.out.println("Four calling birds");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 10:
-                System.out.println("My true love gave to me");
-                System.out.println("Ten lords a leaping");
-                System.out.println("Nine ladies dancing");
-                System.out.println("Eight maids a milking");
-                System.out.println("Seven swans a swimming");
-                System.out.println("Six geese a laying");
-                System.out.println("Five gold rings");
-                System.out.println("Four calling birds");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
-            case 11:
-                System.out.println("My true love gave to me");
-                System.out.println("Eleven pipers piping");
-                System.out.println("Ten lords a leaping");
-                System.out.println("Nine ladies dancing");
-                System.out.println("Eight maids a milking");
-                System.out.println("Seven swans a swimming");
-                System.out.println("Six geese a laying");
-                System.out.println("Five gold rings");
-                System.out.println("Four calling birds");
-                System.out.println("Three French hens");
-                System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
-                break;
+
             case 12:
-                System.out.println("My true love gave to me");
                 System.out.println("Twelve drummers drumming");
+            case 11:
                 System.out.println("Eleven pipers piping");
+            case 10:
                 System.out.println("Ten lords a leaping");
+            case 9:
                 System.out.println("Nine ladies dancing");
+            case 8:
                 System.out.println("Eight maids a milking");
+            case 7:
                 System.out.println("Seven swans a swimming");
+            case 6:
                 System.out.println("Six geese a laying");
+            case 5:
                 System.out.println("Five gold rings");
+            case 4:
                 System.out.println("Four calling birds");
+            case 3:
                 System.out.println("Three French hens");
+            case 2:
                 System.out.println("Two turtle doves");
-                System.out.println("And a partridge in a pear tree");
+                System.out.print("And ");
+            case 1:
+//                if (i == 1) {
+//                     firstLetter = 'A';
+//                }else firstLetter = 'a';
+                char firstLetter = i == 1 ? 'A' : 'a';
+                System.out.println(firstLetter + " partridge in a pear tree");
                 break;
-
-
-        }
+            }
 
         }
     }
