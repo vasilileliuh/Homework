7af4ea64c7887c3828d8bc8e95a0c68de0014ad7
vasilileliuh <vasili.leliuh@gmail.com>
2019-08-16 16:32:07 +0300

    solved with help of boolean

--- exercise3/src/com/company/Main.java
+++ exercise3/src/com/company/Main.java
@@ -5,32 +5,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter one of the following choices: ");
-        System.out.println("a) carnivore            b) pianist ");
-        System.out.println("c) tree                 d) game ");
-        char letter = 'z';
-        do {
-            String choicesABCD = in.nextLine().toLowerCase();
-            letter = choicesABCD.charAt(0);
-            System.out.println("Please enter character from: a, b, c or d: ");
+        boolean running = true;
+        boolean inputIsIncorrect = true;
+        int c = '0';
+
+        while(running){
+            System.out.println("Please enter one of the following choices: ");
+            System.out.println("a) carnivore            b) pianist ");
+            System.out.println("c) tree                 d) game ");
+            System.out.println();
+            while (inputIsIncorrect){
+                c = in.nextLine().toLowerCase().charAt(0);
+                if (c == 'a' || c == 'b' || c == 'c' || c == 'd'){
+                    inputIsIncorrect = false;
+                    running = false;
+                }else if (c == '$') {
+                    inputIsIncorrect = false;
+                    running = false;
+                }else
+                    System.out.println("Input not correct, enter a b c or d:");
+            }
         }
-        while (letter != 'a');
-
-
-        switch (letter) {
+        switch (c) {
             case 'a':
-                System.out.printf("You choose  %c variant. \n", letter);
+                System.out.printf("You choose  %c variant. \n", c);
                 break;
             case 'b':
-                System.out.printf("You choose  %c variant. \n", letter);
+                System.out.printf("You choose  %c variant. \n", c);
                 break;
             case 'c':
-                System.out.printf("You choose  %c variant. \n", letter);
+                System.out.printf("You choose  %c variant. \n", c);
                 break;
             case 'd':
-                System.out.printf("You choose  %c variant. \n", letter);
+                System.out.printf("You choose  %c variant. \n", c);
                 break;
             }
     }
