1995a690a0e4400ec78c2fa45b29e0a6190e34cc
vasilileliuh <vasili.leliuh@gmail.com>
2019-11-17 18:39:48 +0200

    removed redundant String input and throws IOException in exercise AccessKeyApp

--- HW_vanar_15.11/Exercise3/src/com/company/AccessKeyApp.java
+++ HW_vanar_15.11/Exercise3/src/com/company/AccessKeyApp.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 public class AccessKeyApp {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         final long ACCESS_KEY = 0xFEDCBA987654321L;
         final long ACCESS_KEY_UNSIGNED = 0xFEDCBA9876543210L;
@@ -14,13 +14,13 @@ public class AccessKeyApp {
 
         System.out.println("Please enter your key: ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String input = reader.readLine();
 
         try {
-            key = Long.parseUnsignedLong(input, 16);
+            key = Long.parseUnsignedLong(reader.readLine(), 16);
             System.out.println((key == ACCESS_KEY || key == ACCESS_KEY_UNSIGNED) ? "Access Granted!" : "Access Denied!");
         } catch (Exception e) {
-            System.out.println("Something went wrong.\nAccess Denied!");
+            System.out.println("Something went wrong.\n" +
+                    "Access Denied!");
         }
     }
 }
