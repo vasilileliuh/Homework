b3146dbbd414f9b43fd476defa4ed2d50920d272
vasilileliuh <vasili.leliuh@gmail.com>
2019-11-28 15:56:38 +0200

    added exercise WeatherTemperatureApp

---
+++ HW_vanar_28.11/Exercise1/src/com/company/WeatherTemperatureApp.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class WeatherTemperatureApp {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Please enter a valid code\n(one of these countries: Mexico: 484; Micronesia: 583; Moldova: 498; Monaco: 492; Mongolia: 496)");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int countryCode = Integer.parseInt(reader.readLine());
+
+        System.out.print("Temperature: ");
+        System.out.printf("%s%n", isCountryCodValid(countryCode) ? PrivateWeatherForecastProvider.getCurrentTemperature() : "Can't provide data for this country!");
+        System.out.print("Humidity: ");
+        System.out.printf("%s%n", isCountryCodValid(countryCode) ? PrivateWeatherForecastProvider.getCurrentHumidity() : "Can't provide data for this country!");
+
+    }
+
+    private static boolean isCountryCodValid(int countryCode) {
+        return countryCode == 484 || countryCode == 583 ||
+                countryCode == 498 || countryCode == 492 || countryCode == 496;
+
+    }
+}
+
+class OpenWeatherForecastProvider {
+
+    static double getCurrentTemperature() {
+        Random r = new Random();
+        return Math.round(-50.0 + (100) * r.nextDouble());
+    }
+}
+
+class PrivateWeatherForecastProvider extends OpenWeatherForecastProvider {
+
+    static byte getCurrentHumidity() {
+
+        Random r = new Random();
+        return (byte) r.nextInt(101);
+
+    }
+}
